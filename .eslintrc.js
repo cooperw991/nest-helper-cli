d8b10122e64578e5a4286165dc743ee2a113b479
cooperw991 <wangjx@blackbeanssoft.com>
2021-04-13 03:21:36 +0800

    Update eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,24 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'standard'
-  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 12,
+    project: 'tsconfig.json',
     sourceType: 'module'
   },
-  plugins: [
-    '@typescript-eslint'
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
   ],
+  root: true,
+  env: {
+    node: true,
+    jest: true
+  },
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
   }
 }
